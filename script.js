ff6856c86eb35b7a9c270ae5b52a5fc245b718b5
Nazareno <nazareno7870@gmail.com>
2022-01-11 15:49:10 -0300

    Added dekstop mode styling and finalized js for accordion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let active = ""
 window.addEventListener('DOMContentLoaded',(event)=>{
     const items = document.getElementsByTagName("h4")
     for (let item of items){
@@ -11,9 +12,25 @@ window.addEventListener('DOMContentLoaded',(event)=>{
 })
 
 const handleClick = event =>{
+    const id = event.path[2].id
+    console.log(id)
+    console.log(active)
+    id !== active && resetDefault()
     const content = event.path[2].childNodes[3];
     content.classList.toggle('visible')
     const arrow = event.path[1];
     arrow.classList.toggle('active')
+    active = id
 }
 
+const resetDefault = ()=>{
+    const items = document.getElementsByClassName("content")
+    for (let item of items){
+        item.classList.remove('visible')
+    }
+
+    const imgs = document.getElementsByClassName("contain-item")
+    for (let item of imgs){
+        item.classList.remove('active')
+    }
+}
