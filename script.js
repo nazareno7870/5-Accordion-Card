c5ac77c64f78d1becb4c39ea68bddfbea86be457
Nazareno <nazareno7870@gmail.com>
2022-01-10 21:47:29 -0300

    Added js script

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+    const items = document.getElementsByTagName("h4")
+    for (let item of items){
+        item.addEventListener('click',handleClick)
+    }
+
+    const imgs = document.getElementsByClassName("arrow")
+    for (let item of imgs){
+        item.addEventListener('click',handleClick)
+    }
+})
+
+const handleClick = event =>{
+    const content = event.path[2].childNodes[3];
+    content.classList.toggle('visible')
+    const arrow = event.path[1];
+    arrow.classList.toggle('rotate')
+}
+
